1418697a607f4f1f6204a35af31fd28f1fccc65d
Albann0 <131862757+Albann0@users.noreply.github.com>
2024-02-15 15:05:48 +0100

    j

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,10 @@
 <header>
-    
+    <img alt="Logo ITalent" src="assets/Logo.png">
+    <nav>
+        <ul>
+            <li>Inscription</li>
+            <li>Accueil</li>
+            <li>Connexion</li>
+        </ul>
+    </nav>
 </header>
