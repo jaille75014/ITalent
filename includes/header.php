1b323c84816d61cfdf11b88d678bd62dd17b9841
Albann0 <131862757+Albann0@users.noreply.github.com>
2024-02-15 15:50:24 +0100

    header

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,13 @@
 <header>
-    <img alt="Logo ITalent" src="assets/Logo.png">
-    <nav>
-        <ul>
-            <li>Inscription</li>
-            <li>Accueil</li>
-            <li>Connexion</li>
-        </ul>
-    </nav>
+    <div class="header_div">
+        <img alt="Logo ITalent" src="assets/Logo.png" width="100px">
+        <nav>
+            <ul>
+                <li>Inscription</li>
+                <li>Accueil</li>
+                <li>Connexion</li>
+            </ul>
+        </nav>
+        <img alt="Icône Mode Sombre" src="assets/bxs-moon.svg" width="40px">
+    </div>
 </header>
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
         <meta charset="UTF-8">
         <meta name="Description" content="ITalent, la révolution de la recherche d'emplois pour les étudiants en Informatique.">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
         <link type="text/css" rel="stylesheet" href="CSS/style.css">
         <title>ITalent | Accueil</title>
 
