6d8712eed2ec1ffc8ab0e8d07f8d8ca7885fdfd9
jaille75014 <jules91@gmx.com>
2024-02-19 11:14:12 +0100

    commit

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,27 @@
 <header>
+
     <div class="header_div">
-        <img alt="Logo ITalent" src="assets/Logo.png" width="100px">
-        <nav>
+
+        <div class="logo">
+            <img alt="Logo ITalent" src="assets/logo_blanc.png" width="100px">
+        </div>
+
+        <nav class="nav_links">
             <ul>
-                <li>Inscription</li>
-                <li>Accueil</li>
-                <li>Connexion</li>
+                <li><a href="inscription.php">Inscription</a></li>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="connexion.php">Connexion</a></li>
             </ul>
         </nav>
-        <img alt="Icône Mode Sombre" src="assets/bxs-moon.svg" width="40px">
+
+        <div class="dark_icon">
+            <img alt="Icône Mode Sombre" src="assets/dark.svg" width="40px">
+        </div>
+
+        <div class="log-out_icon">
+            <img alt="Icône Log-Out" src="assets/log-out.svg" width="40px">
+        </div>
+
     </div>
+    
 </header>
