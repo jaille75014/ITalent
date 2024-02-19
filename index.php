baaeed9c5a6ae0ea34d977b3c8fa9a4689535215
jaille75014 <jules91@gmx.com>
2024-02-19 16:23:13 +0100

    commit

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <meta name="Description" content="ITalent, la révolution de la recherche d'emplois pour les étudiants en Informatique.">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="CSS/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/litera/bootstrap.min.css" integrity="sha384-enpDwFISL6M3ZGZ50Tjo8m65q06uLVnyvkFO3rsoW0UC15ATBFz3QEhr3hmxpYsn" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.css" integrity="sha384-enpDwFISL6M3ZGZ50Tjo8m65q06uLVnyvkFO3rsoW0UC15ATBFz3QEhr3hmxpYsn" crossorigin="anonymous">
     <title>ITalent | Accueil</title>
 </head>
 
@@ -29,15 +29,16 @@
             Donnes nous juste ton CV, prouves tes compétences avec des tests et on se charge du reste ! <br>
             </p>
 
-            <div class="d-grid gap-2">
-                <a href="inscription.php" class="btn btn-lg btn-primary" role="button">
-                    <span style="color: #FEE715;" class="btn-text">Viens décrocher ton prochain emploi !</span>
+            <div class="bouton_emploi">
+                <a href="inscription.php" class="btn btn-primary">
+                    Viens décrocher ton prochain emploi !
                 </a>
             </div>
 
 
         </div>
     </div>
+
 
 
     <div class="container">
