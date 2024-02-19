6b97fbdbeabcc68386fe1650e278a8fabd3ad6fe
jaille75014 <jules91@gmx.com>
2024-02-19 15:12:30 +0100

    commit

--- index.php
+++ index.php
@@ -16,27 +16,57 @@
     <?php include('includes/header.php');?>
 
     <div class="banner">
+        <div class="banner_content">
+            <h2>Prouve tes compétences, <br> 
+            Discute, <br>
+            Passe un entretien et... <br>
+            Décroche le job ! <br>
+            </h2>
 
-    <div class="banner_content">
-        <h2>Prouve tes compétences, <br> 
-        Discute, <br>
-        Passe un entretien et... <br>
-        Décroche le job ! <br>
-        </h2>
+            <p>Bienvenue chez <span style="color: #FEE715; font-weight: bold">Italent</span> ! <br>
+            LA plateforme d’emploi spécialement conçue <br>
+            pour les étudiants. Ici c’est aux recruteurs de te contacter ! <br>
+            Donnes nous juste ton CV, prouves tes compétences avec des tests et on se charge du reste ! <br>
+            </p>
 
-        <p>Bienvenue chez <span style="color: #FEE715; font-weight: bold">Italent</span> ! <br>
-        LA plateforme d’emploi spécialement conçue <br>
-        pour les étudiants. Ici c’est aux recruteurs de te contacter ! <br>
-        Donnes nous juste ton CV, prouves tes compétences avec des tests et on se charge du reste ! <br>
-        </p>
-
-        <div class="d-grid gap-2">
-        <button class="btn btn-lg btn-primary" type="button"><span style="color: #FEE715">Viens décrocher ton prochain emploi !</span></button>
+            <div class="d-grid gap-2">
+            <button class="btn btn-lg btn-primary" type="button"><span style="color: #FEE715">Viens décrocher ton prochain emploi !</span></button>
+            </div>
         </div>
     </div>
 
 
+    <div class="container">
+        <div class="student">
+            <h3>ETUDIANT</h3>
+            <ul>
+                <li>Passage de compétences 1</li>
+                <li>Création de CV sur mesure</li>
+                <li>Interaction avec les recruteurs</li>
+                <li>Posts et storys pour accroître la visibilité</li>
+                <li>Inscription simple et rapide</li>
+            </ul>
+        </div>
+        <div class="recruiter">
+            <h3>RECRUTEUR</h3>
+            <ul>
+                <li>Recherche simple et rapide de talents</li>
+                <li>Engage la discussion avec les étudiants</li>
+                <li>Création de compte rapidement</li>
+                <li>Abonnements aux comptes étudiants</li>
+                <li>Recommendations de talents</li>
+            </ul>
+        </div>
     </div>
+
+
+
+
+
+
+
+
+    <?php include('includes/footer.php');?>
 
 
 </body>
