8e3f0d8f307590f1a678dd686ea6999a98954a05
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-02-21 12:10:58 +0100

    modified:   includes/footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,11 +2,11 @@
 
 <footer class="bg-primary text-center text-lg-start text-white">
     <!-- Grid container -->
-<div class="container p-4">
+<div class="container-fluid p-4">
     <!-- Grid row -->
     <div class="row my-4">
     <!--Grid column-->
-    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+    <div class="col-lg-3 col-md-5 mb-4 mb-md-0">
         <div class="shadow-1-strong d-flex align-items-center justify-content-center mb-4 mx-auto" style="width: 150px; height: 150px;">
             <img src="assets/man_working.png" height="150px" alt="man working on a computer" loading="lazy">
         </div>
@@ -33,10 +33,10 @@
     <!-- Grid column -->
 
     <!-- Une autre colonne -->
-    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+    <div class="col-lg-4 col-md-5 mb-4 mb-md-0">
         <h5 class="text-uppercase mb-4">Italent</h5>
         <ul class="list-unstyled">
-            <li class="mb-2">
+            <li class="mb-1">
               <a href="#!" class="text-white">A propos de nous</a>
             </li>
             <li class="mb-2">
@@ -50,7 +50,7 @@
     <!-- Grid column -->
     
     <!-- Grid column -->
-    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+    <div class="col-lg-4 col-md-5 mb-4 mb-md-0">
         <h5 class="text-uppercase mb-4">Informations légales</h5>
         <ul class="list-unstyled">
             <li class="mb-2">
@@ -67,17 +67,24 @@
     <!-- Grid column -->
 
     <!-- Grid column -->
-    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+    <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
         <h5 class="text-uppercase mb-4">Nous contacter</h5>
         <ul class="list-unstyled">
             <li class="mb-2">
-              <a href="#!" class="text-white"><box-icon type='solid' name='map'></box-icon>Conditions générales</a>
+              <a href="#!" class="text-white"><i class='bx bxs-map'></i>21 rue Erard, 75012 Paris</a>
             </li>
             <li class="mb-2">
-              <a href="#!" class="text-white">Politique de confidentialité</a>
+              <a href="#!" class="text-white"><i class='bx bxs-phone'></i>+33 1 23 45 67 89</a>
             </li>
             <li class="mb-2">
-              <a href="#!" class="text-white">Utilisation des cookies</a>
+            <form>
+                <div class="form-group">
+                    <label for="">Envoyez nous un mail</label>
+                    <input type="email" class="form-control" name="email" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted">Nous ne partagerons votre e-mail avec personne d'autre !</small>
+                </div>
+            </form>
+              
             </li>
           </ul>
     </div>
