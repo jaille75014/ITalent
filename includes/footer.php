c99c4da427738e02acdd7471aada604d0c4c01e8
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-02-21 10:04:22 +0100

    new file:   assets/man_working.png 	modified:   includes/footer.php

---
+++ includes/footer.php
@@ -0,0 +1,31 @@
+<div class="container">
+
+<footer class="bg-primary text-center text-lg-start text-white">
+    <!-- Grid container -->
+<div class="container p-4">
+    <!-- Grid row -->
+    <div class="row my-4">
+    <!--Grid column-->
+    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <div class="shadow-1-strong d-flex align-items-center justify-content-center mb-4 mx-auto" style="width: 150px; height: 150px;">
+            <img src="../assets/man_working.png" height="70" alt="man working on a computer" loading="lazy">
+
+        </div>
+    <p class="text-center">Le treisième travail d'Hercule : Trouver un emploi.</p>
+
+    <ul class="list-unstyled d-flex flex-row justify-content-center">
+        <li>
+            <a class="text-white px-2" href="#">
+                <!-- Rajouter les logos comme github, insta...  -->
+            </a>
+        </li>
+    </ul>
+    </div>
+
+    </div>
+
+</div>
+
+</footer>
+
+</div>
