4bd3ffaef6b74522f49f556e37b156f0daf4bae2
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-02-23 17:02:56 +0100

    modified:   connexion.php 	modified:   css/style.css 	modified:   includes/footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,56 +1,41 @@
-<div class="container-fluid">
-
-<footer class=" text-center text-lg-start text-white">
-    <!-- Grid container -->
-<div class="container-fluid p-4">
-    <!-- Grid row -->
-    <div class="row my-4">
-    <!--Grid column-->
-    <div class="col-lg-3 col-md-5 mb-4 mb-md-0">
-        <div class="shadow-1-strong d-flex align-items-center justify-content-center mb-4 mx-auto" style="width: 150px; height: 150px;">
-            <img src="assets/man_working.png" height="150px" alt="man working on a computer" loading="lazy">
-        </div>
-    <p class="text-center">Le treisième travail d'Hercule : Trouver un emploi.</p>
-
-    <ul class="list-unstyled d-flex flex-row justify-content-center">
-        <li>
-            <a class="text-white px-2" href="#">
-                <!-- Rajouter les logos comme github, insta...  -->
-            </a>
-        </li>
-        <li>
-            <a class="text-white px-2" href="#!">
-                <!-- logos blablabknaln -->
-            </a>
-            </li>
-            <li>
-              <a class="text-white ps-2" href="#!">
-                <!-- 3eme logo si y'a -->
-              </a>
-            </li>
-    </ul>
+<footer>
+  <div class="row">
+    <div class="col">
+      <img src="assets/man_working.png" class="logo" height="120px">
+      <p>Le treisième travail d'Hercule : Trouver un emploi.</p>
     </div>
-    <!-- Grid column -->
-
-    <!-- Une autre colonne -->
-    <div class="col-lg-2 col-md-5 mb-4 mb-md-0">
-        <h5 class="text-uppercase mb-4">Italent</h5>
+      <div class="col">
+        <h5 class="text-uppercase mb-4">Nous contacter</h5>
         <ul class="list-unstyled">
-            <li class="mb-1">
-              <a href="#!" class="text-white">A propos de nous</a>
+            <li class="mb-2">
+              <a href="#!" class="text-white"><i class='bx bxs-map'></i>21 rue Erard, 75012 Paris</a>
             </li>
             <li class="mb-2">
-              <a href="#!" class="text-white">Mon compte</a>
+              <a href="#!" class="text-white"><i class='bx bxs-phone'></i>+33 1 23 45 67 89</a>
             </li>
+            <li class="mb-2">
+              <a href="#!" class="text-white"><i class='bx bxs-mail'></i>email.email@exemple.fr</a>
+            </li>
+          </ul>
+        </div>
+
+
+        <div class="col">
+        <h5 class="text-uppercase mb-4">Liens</h5>
+        <ul class="list-unstyled">
             <li class="mb-2">
               <a href="#!" class="text-white">Se connecter</a>
             </li>
-        </ul>
+            <li class="mb-2">
+              <a href="#!" class="text-white">S'enregistrer</a>
+            </li>
+            <li class="mb-2">
+              <a href="#!" class="text-white">A propos de nous</a>
+            </li>
+          </ul>
     </div>
-    <!-- Grid column -->
-    
-    <!-- Grid column -->
-    <div class="col-lg-2 col-md-5 mb-4 mb-md-0">
+
+    <div class="col">
         <h5 class="text-uppercase mb-4">Informations légales</h5>
         <ul class="list-unstyled">
             <li class="mb-2">
@@ -64,36 +49,15 @@
             </li>
           </ul>
     </div>
-    <!-- Grid column -->
-
-    <!-- Grid column -->
-    <div class="col-lg-2 col-md-6 mb-4 mb-md-0 offset-md-2">
-        <h5 class="text-uppercase mb-4">Nous contacter</h5>
-        <ul class="list-unstyled">
-            <li class="mb-2">
-              <a href="#!" class="text-white"><i class='bx bxs-map'></i>21 rue Erard, 75012 Paris</a>
-            </li>
-            <li class="mb-2">
-              <a href="#!" class="text-white"><i class='bx bxs-phone'></i>+33 1 23 45 67 89</a>
-            </li>
-            <li class="mb-2">
-            <form method="POST" action="">
-                <input type="email" name="email" placeholder="email">
-              </div>
-            </form>
-              
-            </li>
-          </ul>
-    </div>
-    <!-- Grid column -->
-    
-    </div>
-    <!-- Grid row -->
-</div>
-<!-- Grid container-fluid -->
-
-<!-- Les copyrights
-
+      <div class="col">
+      <h5 class="text-uppercase mb-4">Newsletter</h5>
+      <form action="" method="POST">
+      <i class='bx bxs-envelope'></i>
+      <input type="email" name="email" placeholder="Entrez votre e-mail" required>
+      <button type="submit"><i class='bx bx-right-arrow-alt'></i></button>
+    </form>
+      </div>
+  </div>
+  <hr>
+  <p class="copyright">Tous droits réservés @ 2024 - ITalent </p>
 </footer>
-
-</div>  
