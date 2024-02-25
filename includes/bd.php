394cd26d9d88bf4717f99e01fe97ad44481d2a8a
jaille75014 <jules91@gmx.com>
2024-02-25 15:29:49 +0100

    new file:   assets/banner_admin.jpg 	new file:   includes/bd.php

---
+++ includes/bd.php
@@ -0,0 +1,10 @@
+<?php
+
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=mysql', 'root', 'root');
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
+?>
