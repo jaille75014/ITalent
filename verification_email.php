212da451f1dd13458650663a7e8af23c77419d1b
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-02-28 22:24:03 +0100

    modified:   verification_email.php

--- verification_email.php
+++ verification_email.php
@@ -1,23 +1,58 @@
-<?php 
+<?php
 session_start();
+//Import PHPMailer classes 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 include('includes/bd.php');
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
 $rand_verification_email = rand(100000, 999999);
 echo $_GET['message'];
 
 
-echo $rand_verification_email;
-$to = $_GET['message'];
-$subject = "Confirmation de votre inscription";
-$message = 'Bonjour, nous vous remercions de faire confiance à Italent pour la recherche de votre prochain emploi !
-Nous avons juste besoin d\'une petite vérification de votre part pour que vous puissiez vous connecter. 
-Copiez ce code pour vérifier votre identité et retournez sur Italent !
-Code de vérification :'  . $rand_verification_email;
-$headers = "Content-Type: text/plain; charset=utf-8\r\n";
-$headers = "From: fron.rafael@gmail.com\r\n";
-if(mail($to, $subject, $message, $headers))
-    echo 'Un mail a été envoyé, regardez votre boite mail, nous vous avons envoyé un code';
-else
-    echo 'Erreur';
+try {
+    //Server settings
+    $mail->SMTPDebug = 1;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'AJR';                     //SMTP username
+    $mail->Password   = 'vwxlkgditnwwvlmr';                               //SMTP password
+    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+    $mail->Port       = 587;              //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
+    //Recipients
+    $mail->setFrom('italent.contact.site@gmail.com', 'Italent');
+    $mail->addAddress($_GET['message']); // Destinataire
+
+$body = '<p>Bonjour, nous vous remercions de faire confiance à Italent pour la recherche de votre prochain emploi !
+Nous avons juste besoin d\'une petite vérification de votre part pour que vous puissiez vous connecter. 
+Copiez ce code pour vérifier votre identité et retournez sur Italent !<br>Votre code de vérification :' . $rand_verification_email . '</p>';
+
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Confirmation de votre inscription';
+    $mail->Body    = $body;
+    $mail->AltBody = strip_tags($body);
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+$message = '
+Code de vérification :'  . $rand_verification_email;
+
+
+
+
+    
 ?>
