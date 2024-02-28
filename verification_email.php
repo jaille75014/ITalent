e85c12295736fa77c118d0a43c36802672abe0b9
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-02-28 22:28:37 +0100

    modified:   verification_email.php

--- verification_email.php
+++ verification_email.php
@@ -22,8 +22,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'AJR';                     //SMTP username
-    $mail->Password   = 'vwxlkgditnwwvlmr';                               //SMTP password
+    $mail->Username   = 'italent.contact.site@gmail.com';                     //SMTP username
+    $mail->Password   = 'AJR3MOUSQUETAIRES';                               //SMTP password
     $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
     $mail->Port       = 587;              //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
