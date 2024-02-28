e061aea4c55a0b329e6b40fb016f2ad99a9a2fde
jaille75014 <jules91@gmx.com>
2024-02-28 20:09:08 +0100

    modified:   css/style.css 	new file:   index_recruteur.php

---
+++ index_recruteur.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil | ITalent</title>
+    <meta name="Description" content="ITalent, la révolution de la recherche d'emplois pour les étudiants en Informatique.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Intégration de la police d'écriture  -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <!-- Icône de Boxincons -->
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <!-- Intégration Bootstrap 5  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Intégration de notre CSS -->
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+
+</head>
+
+    <body class="bg-light">
+
+    <?php include('includes/header.php');?>
+
+    <main class="bg-light">
+        <div class="banner-recruiter">
+            <img src="assets/banner_recruteur.jpg" alt="Bannière recruteur">
+        </div>
+
+        <div class="row py-5 gy-4">
+            <h1 class="text-center">CV Etudiants</h1>
+        </div>
+                
+    
+  
+  
+
+
+
+
+
+
+
+
+        <script src="js/script.js"></script>
+        
+        </main>
+
+        <?php include('includes/footer.php');?>
+    
+    </body>
+</html>
