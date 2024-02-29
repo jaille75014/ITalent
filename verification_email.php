2e544bd035c435726a60b3fb864b9e40a8929bcc
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-02-29 19:15:17 +0100

    modified:   verification_email.php

--- verification_email.php
+++ verification_email.php
@@ -5,7 +5,25 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 include('includes/bd.php');
-
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Intégration de la police d'écriture  -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <!-- Intégration Bootstrap 5  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Intégration de notre CSS -->
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <title>Verification | Italent</title>
+</head>
+<body>
+    <?php include('includes/header.php') ?>
+    <main>
+    
 //Load Composer's autoloader
 require 'vendor/autoload.php';
 
@@ -50,23 +68,6 @@ try {
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Intégration de la police d'écriture  -->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-    <!-- Intégration Bootstrap 5  -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <!-- Intégration de notre CSS -->
-    <link type="text/css" rel="stylesheet" href="css/style.css">
-    <title>Verification | Italent</title>
-</head>
-<body>
-    <?php include('includes/header.php') ?>
-    <main>
         <h1>Vous y êtes presque !</h1>
         <p>Nous vous avons envoyé un mail de confirmation à l'adresse mail que vous avez indiqué dans le formulaire
             consultez la et revenez ici le plus vite possible !
