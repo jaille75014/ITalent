84ce7350c2d94b8c962db213fdc128920d415961
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-03-01 16:12:03 +0100

    modified:   verification_email.php

--- verification_email.php
+++ verification_email.php
@@ -13,7 +13,7 @@ $mail = new PHPMailer(true);
 
 $rand_verification_email = rand(100000, 999999);
 $q = 'UPDATE USERS
-SET email_number = $rand_verification_email
+SET email_number = '. $rand_verification_email .';
 WHERE email = \''. htmlspecialchars($_GET['message']).'\'';
 
 try {
