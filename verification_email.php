63eed4c2e32aa7943eb693802bb5e6d34f67b3a9
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-03-01 13:21:03 +0100

    modified:   verification_email.php

--- verification_email.php
+++ verification_email.php
@@ -5,25 +5,6 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 include('includes/bd.php');
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Intégration de la police d'écriture  -->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-    <!-- Intégration Bootstrap 5  -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <!-- Intégration de notre CSS -->
-    <link type="text/css" rel="stylesheet" href="css/style.css">
-    <title>Verification | Italent</title>
-</head> 
-<body>
-    <?php include('includes/header.php'); ?>
-    <main>
-    <?php
 //Load Composer's autoloader
 require 'vendor/autoload.php';
 
@@ -79,9 +60,7 @@ try {
         <input type="text" name="code" placeholder="Entrez le code :">
         <input type="submit" value="Vérifier le code">
         </form>
-    </main>
-</body>
-</html>
+
 
         <?php 
         $q = 'SELECT email_number FROM USERS WHERE email = ' . htmlspecialchars($_GET['mail']); 
@@ -95,4 +74,4 @@ try {
             header('location: inscription.php?messageFailure=Reessaye !');
             exit;
         }
-        ?>
+?>
