8e7409133768530cef3a841c6552b50cb350fae6
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-03-05 11:38:14 +0100

    modified:   codes_verification.php

--- codes_verification.php
+++ codes_verification.php
@@ -4,21 +4,20 @@ if(!isset($_POST['code']) || empty($_POST['code']) || !isset($_POST['email']) ||
     header('location: verification_email.php?message=Vous vous êtes trompé dans l\'écriture du code ou de votre email');
     exit;
     }
-        $q = 'SELECT email_number FROM USERS WHERE email = :email'; 
-        $req=$bdd->prepare($q);
-            $result=$req->execute([
-            'email' => '\'' . htmlspecialchars($_POST['email']). '\'',
-            ]);   
-            $results=$req->fetchAll();
-            echo'Result :' . $results . 'OU :' . $req;
+    $q = 'SELECT email_number FROM USERS WHERE email = :email';
+    $req = $bdd->prepare($q);
+    $result = $req->execute([
+        'email' => htmlspecialchars($_POST['email'])
+    ]);
+    $results = $req->fetch();
         // Vérifie si le code correspond à celui inscrit dans la bdd
-        if($_POST['code'] == $results['email_number']){
+        if ($_POST['code'] == $results['email_number']) {
             // Si c'est le cas, on valide l'email
             $q = 'UPDATE USERS SET email_check = 1 WHERE email = :email';
             $req = $bdd->prepare($q);
             $result = $req->execute([
-                'email' => '\'' . htmlspecialchars($_POST['email']). '\''
-            ]);   
+                'email' => htmlspecialchars($_POST['email'])
+            ]);  
             header('location: connexion.php?messageSuccess=Inscription valide, veuillez vous connecter');
             exit;
         } else {
