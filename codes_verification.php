cd8108b75aa4d28241b8c190a83edf0bcb3f0e0d
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-03-05 11:31:22 +0100

    modified:   codes_verification.php

--- codes_verification.php
+++ codes_verification.php
@@ -1,9 +1,8 @@
 <?php
 include("includes/bd.php");
-    if(!isset($_POST['code'])
-    || empty($_POST['code'])){
-        header('location: verification_email.php?message=Vous vous êtes trompé dans l\'écriture du code ou de votre email');
-        exit;
+if(!isset($_POST['code']) || empty($_POST['code']) || !isset($_POST['email']) || empty($_POST['email'])){
+    header('location: verification_email.php?message=Vous vous êtes trompé dans l\'écriture du code ou de votre email');
+    exit;
     }
         $q = 'SELECT email_number FROM USERS WHERE email = :email'; 
         $req=$bdd->prepare($q);
@@ -13,7 +12,7 @@ include("includes/bd.php");
             $results=$req->fetchAll();
             echo'Result :' . $results . 'OU :' . $req;
         // Vérifie si le code correspond à celui inscrit dans la bdd
-        if($_POST['code'] === $results['email_number']){
+        if($_POST['code'] == $results['email_number']){
             // Si c'est le cas, on valide l'email
             $q = 'UPDATE USERS SET email_check = 1 WHERE email = :email';
             $req = $bdd->prepare($q);
