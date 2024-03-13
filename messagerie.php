6f32008b092ac27b7f80ba9a4510dbf4d0984086
Albann0 <131862757+Albann0@users.noreply.github.com>
2024-03-13 10:23:17 +0100

    a

--- messagerie.php
+++ messagerie.php
@@ -11,6 +11,8 @@
     <title>Messagerie | ITalent</title>
     <meta name="Description" content="ITalent, la révolution de la recherche d'emplois pour les étudiants en Informatique.">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Ajout de la favicon -->
+    <link rel="icon" type="image/png" href="assets/LOGO_icone.png">
     <!-- Intégration Bootstrap 5  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
