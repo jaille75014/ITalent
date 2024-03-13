8149c88a3816a2fc4e00d2ec0e33ab04b6da920a
Albann0 <131862757+Albann0@users.noreply.github.com>
2024-03-13 17:53:20 +0100

    include head

--- inscription.php
+++ inscription.php
@@ -1,18 +1,9 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Ajout de la favicon -->
-        <link rel="icon" type="image/png" href="assets/LOGO_icone.png">
-
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link type="text/css" rel="stylesheet" href="css/style.css">
-        <title>Inscription | ITalent</title>
-    </head>
+<?php 
+$title='Inscription';
+include('includes/head.php');?>
 
     <body class="bg-light">
         
