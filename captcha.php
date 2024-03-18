a4a6ec3a14a534719f4f9c8a856565f88c8f1021
Albann0 <131862757+Albann0@users.noreply.github.com>
2024-03-18 15:48:19 +0100

    l

--- captcha.php
+++ captcha.php
@@ -18,10 +18,47 @@ $q = 'SELECT COUNT(question_id) FROM captcha;';
 $req = $bdd->prepare($q);
 $req->execute();
 $result = $req->fetch(PDO::FETCH_ASSOC);
-foreach($result as $index => $value) $number=$value;
+foreach($result as $index => $value) $numberMax=$value;
 
-$id_question=rand(1, $number);
+$numberQuestion=rand(1, $numberMax);
 
+$q = 'SELECT question,answer FROM captcha;';
+$req = $bdd->prepare($q);
+$req->execute();
+$result = $req->fetch(PDO::FETCH_ASSOC);
+
+for ($i=1;$i<=$numberQuestion;$i++){
+    if ($i==$numberQuestion){
+        $question=$result['question'];
+        $answer=$result['answer'];
+    } else {
+        $result = $req->fetch(PDO::FETCH_ASSOC);
+    }
+}
+
+if(isset($_POST['answer'])&& !empty($_POST['answer'])){
+    if ($_POST['answer']== $answer){
+        switch($_SESSION['statut']){
+            case 1 : 
+                header('location:etudiant.php');
+                exit;
+                break;
+            case 2 : 
+                header('location:index_recruteur.php');
+                exit;
+                break;
+            case 3 : 
+                header('location:admin.php');
+                exit;
+                break;
+        }
+    } else {
+        header('location:captcha.php?error=Veuillez réessayer !');
+        exit;
+    }
+}
+
+    
 
 ?>
 
@@ -44,10 +81,28 @@ $id_question=rand(1, $number);
                     if(isset($_GET['messageSuccess'])){
                         echo '<div class="alert alert-success" role="alert">'.htmlspecialchars($_GET['messageSuccess']).'</div>'; 
                     }
-
-                    echo $id_question;
+                    if(isset($_GET['error'])){
+                        echo '<div class="alert alert-danger" role="alert">'.htmlspecialchars($_GET['error']).'</div>'; 
+                        }
+                    
                     
                 ?>
+
+                <div class="row">
+                    <div class="col-4 offset-4">
+                        <div class="card my-5">
+                            <div class="card-body">
+                                <form method="post">
+                                    <label for="question" class="form-label"><?= $question?></label>
+                                    <input type="text" placeholder="Votre réponse" class="form-control" name="answer" id="question">
+                                    <button class="btn btn-primary mt-4" type="submit">Soumettre</button>
+                                </form>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
 
 
                 
