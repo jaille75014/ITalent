ed20981652a90437be4178aeb0cb2a79c19727f3
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-03-18 11:12:59 +0100

    modified:   includes/bd.php

--- includes/bd.php
+++ includes/bd.php
@@ -4,7 +4,7 @@ try {
     $bdd = new PDO('mysql:host=localhost:3306;dbname=italent', 'root', 'root');
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die('Erreur : ' . $e->getMessage());
+    die('Erreur :' . $e->getMessage());
 }
 
 ?> 
