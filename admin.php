b747af91217205e51f326ec2a01a339406fb39da
jaille75014 <jules91@gmx.com>
2024-03-22 18:51:27 +0100

    modified:   admin.php 	modified:   back/log_reussies.txt

--- admin.php
+++ admin.php
@@ -71,102 +71,93 @@ include('includes/head.php');?>
 
 <body class="bg-light">
 
-    <?php include('includes/header.php');?>
-
+    <?php include('includes/header.php'); ?>
 
     <div class="container">
 
         <h3>Rechercher un utilisateur :</h3>
 
         <div class="mb-3">
-            <form method="post" class="form-inline">
-                <div class="form-group mr-2">
-                    <label for="user_id" class="mr-2">ID :</label>
-                    <input type="text" name="user_id" id="user_id" class="form-control" value="<?php echo $user_id; ?>">
+            <form method="post">
+                <div class="row align-items-end"> 
+                    <div class="col-12 col-sm-2 col-md-1 form-group">
+                        <label for="user_id">ID :</label>
+                        <input type="text" name="user_id" id="user_id" class="form-control" value="<?php echo $user_id; ?>">
+                    </div>
+                    <div class="col-12 col-sm-4 col-md-5 form-group"> 
+                        <label for="email">Email :</label>
+                        <input type="text" name="email" id="email" class="form-control" value="<?php echo $email; ?>">
+                    </div>
+                    <div class="col-6 col-sm-3 col-md-2 form-group">
+                        <label for="lastname">Nom :</label>
+                        <input type="text" name="lastname" id="lastname" class="form-control" value="<?php echo $lastname; ?>">
+                    </div>
+                    <div class="col-6 col-sm-3 col-md-2 form-group">
+                        <label for="firstname">Prénom :</label>
+                        <input type="text" name="firstname" id="firstname" class="form-control" value="<?php echo $firstname; ?>">
+                    </div>
+                    <div class="col-12 col-md-2 mt-3 mt-md-0">
+                        <button type="submit" class="btn btn-primary w-100">Rechercher</button>
+                    </div>
                 </div>
-                <div class="form-group mr-2">
-                    <label for="email" class="mr-2">Email :</label>
-                    <input type="text" name="email" id="email" class="form-control" value="<?php echo $email; ?>">
-                </div>
-                <div class="form-group mr-2">
-                    <label for="lastname" class="mr-2">Nom :</label>
-                    <input type="text" name="lastname" id="lastname" class="form-control" value="<?php echo $lastname; ?>">
-                </div>
-                <div class="form-group mr-2">
-                    <label for="firstname" class="mr-2">Prénom :</label>
-                    <input type="text" name="firstname" id="firstname" class="form-control" value="<?php echo $firstname; ?>">
-                </div>
-                <button type="submit" class="btn btn-primary">Rechercher</button>
             </form>
         </div>
 
-
-
-
-            <div class="table-responsive">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th class="text-center">Id</th>
-                            <th class="text-center">Nom</th>
-                            <th class="text-center">Prénom</th>
-                            <th class="text-center">Email</th>
-                            <th class="text-center">Statut</th>
-                            <th class="text-center">Supprimer publications</th>
-                            <th class="text-center">Supprimer story</th>
-                            <th class="text-center">Action</th>
-                            <th class="text-center">Raison bannissement</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($users as $user): ?>
-                        <tr>
-                            <td class="text-center"><?php echo $user['user_id']; ?></td>
-                            <td class="text-center"><?php echo $user['lastname']; ?></td>
-                            <td class="text-center"><?php echo $user['firstname']; ?></td>
-                            <td class="text-center"><?php echo $user['email']; ?></td>
-                            <td class="text-center"><?php if($user['statut'] == '1') {
-                                echo "Etudiant";
-                                } else if ($user['statut'] == '2') {
-                                    echo "Recruteur";
-                                } else {
-                                    echo "Admin"; }?></td>
-                            <td class="text-center">
-                                <form method="post">
-                                    <input type="hidden" name="user_id" value="<?php echo $user['user_id']; ?>">
-                                    <button type="submit" class="btn btn-danger" name="delete_publications">Supprimer</button>
-                                </form>
-                            </td>
-                            <td class="text-center">
-                                <form method="post">
-                                    <input type="hidden" name="user_id" value="<?php echo $user['user_id']; ?>">
-                                    <button type="submit" class="btn btn-danger" name="delete_story">Supprimer</button>
-                                </form>
-                            </td>
-                            <td class="text-center">
-                                <form method="post">
-                                    <input type="hidden" name="user_id" value="<?php echo $user['user_id']; ?>">
-                                    <button type="submit" class="btn btn-danger" name="delete_user">Bannir</button>
-                                </form>
-                            </td>
-                            <td class="text-center">
-                                <form class="d-flex" method="post">
-                                    <input type="hidden" name="user_id" value="<?php echo $user['user_id']; ?>">
-                                    <input type="text" name="raison_suppression" class="form-control me-2" placeholder="Raison :" required>
-                                    <button type="submit" class="btn btn-primary">Valider</button>
-                                </form>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
-    
-
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th class="text-center">Id</th>
+                        <th class="text-center">Nom</th>
+                        <th class="text-center">Prénom</th>
+                        <th class="text-center">Email</th>
+                        <th class="text-center">Statut</th>
+                        <th class="text-center">Supprimer publications</th>
+                        <th class="text-center">Supprimer story</th>
+                        <th class="text-center">Action</th>
+                        <th class="text-center">Raison bannissement</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($users as $user): ?>
+                    <tr>
+                        <td class="text-center"><?php echo $user['user_id']; ?></td>
+                        <td class="text-center"><?php echo $user['lastname']; ?></td>
+                        <td class="text-center"><?php echo $user['firstname']; ?></td>
+                        <td class="text-center"><?php echo $user['email']; ?></td>
+                        <td class="text-center"><?php echo $user['statut'] == '1' ? "Etudiant" : ($user['statut'] == '2' ? "Recruteur" : "Admin"); ?></td>
+                        <td class="text-center">
+                            <form method="post">
+                                <input type="hidden" name="user_id" value="<?php echo $user['user_id']; ?>">
+                                <button type="submit" class="btn btn-danger" name="delete_publications">Supprimer</button>
+                            </form>
+                        </td>
+                        <td class="text-center">
+                            <form method="post">
+                                <input type="hidden" name="user_id" value="<?php echo $user['user_id']; ?>">
+                                <button type="submit" class="btn btn-danger" name="delete_story">Supprimer</button>
+                            </form>
+                        </td>
+                        <td class="text-center">
+                            <form method="post">
+                                <input type="hidden" name="user_id" value="<?php echo $user['user_id']; ?>">
+                                <button type="submit" class="btn btn-danger" name="delete_user">Bannir</button>
+                            </form>
+                        </td>
+                        <td class="text-center">
+                            <form class="d-flex flex-column flex-md-row" method="post">
+                                <input type="hidden" name="user_id" value="<?php echo $user['user_id']; ?>">
+                                <input type="text" name="raison_suppression" class="form-control me-md-2 mb-2 mb-md-0" placeholder="Raison :" required>
+                                <button type="submit" class="btn btn-primary">Valider</button>
+                            </form>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
         </div>
-    </main>
+    </div>
 
-    <?php include('includes/footer.php');?>
+<?php include('includes/footer.php'); ?>
 
 </body>
-</html>
