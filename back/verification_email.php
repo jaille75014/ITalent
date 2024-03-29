30462b4f821e11c6c7cbbf1fd040303d5caff10f
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-03-29 22:01:48 +0100

    modified:   back/verification_email.php

--- back/verification_email.php
+++ back/verification_email.php
@@ -2,8 +2,10 @@
 session_start();
 $_SESSION['code'] = '1';
 include('../includes/bd.php');
-
 include('../includes/phpmailer.php');
+include('../includes/head.php');
+include('../includes/header.php');
+
 
     $rand_verification_email = rand(1000000, 9999999); // Genère une valeur à 7 chiffres
     $select_id = 'SELECT user_id FROM USERS WHERE email = :email';
@@ -15,7 +17,7 @@ include('../includes/phpmailer.php');
 
     $id_user = $result['user_id'];
     
-    $date = date('Y-m-d H:i:s', strtotime('1 hour')); // Add one hour to the actual date
+    $date = date('Y-m-d H:i:s', strtotime('1 hour')); // Rajoute une heure pour stocker l'expiration
 
     $token = 'INSERT INTO TOKEN (value, date, user_id) values (:value, :date, :user_id)';
     $req=$bdd->prepare($token);
@@ -32,7 +34,9 @@ include('../includes/phpmailer.php');
 
     $body = '<p>Bonjour, nous vous remercions de faire confiance à Italent pour la recherche de votre prochain emploi ! <br><br>
     Nous avons juste besoin d\'une petite vérification de votre part pour que vous puissiez vous connecter. <br>
-    Cliquez sur ce lien pour vérifier votre identitée : <a href="https://italent.site/back/codes_verification.php?id=' . $id_user . '&token=' . $rand_verification_email . '&check=0">Clique vite !</a><br>
+    Copiez ce code : <br></p>
+    <h3>' . $rand_verification_email . '</h3>
+    <p>Et cliquez sur ce lien pour vérifier votre identitée : <a href="https://italent.site/back/codes_verification.php?id=' . $id_user . '&token=' . $rand_verification_email . '&check=0">Clique vite !</a><br>
     <b>Attention !</b> Ce lien n\'est valable que pendant 1h! </p>';
 
 
@@ -51,19 +55,40 @@ include('../includes/phpmailer.php');
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     } 
-?>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+
+    // Code pour le reload de la page
+
+    if(isset($_GET['reload'])){
+        $check_token = 'SELECT email_check FROM USERS WHERE email = :email';
+        $req = $bdd->prepare($check_token);
+        $result = $req->execute([
+            'email'=> htmlspecialchars($_GET['message'])
+        ]);
+        $result = $req->fetch(PDO::FETCH_ASSOC);
+        if($result['email_check'] = 1){
+            header('location: ../connexion.php?messageSuccess=Votre email a été vérifié, veuillez vous connecter');
+        }
+    }
+
+
+?>
+<script>
+    var timer = null;
+    function auto_reload()
+    {
+        window.location = 'https://italent.site/back/verification_email.php?reload=1';  //your page location
+    }
+
+</script> 
+
+
+<!-- Recharge la page toutes les 10 seconds. -->
+<body onload="timer = setTimeout('auto_reload()',10000);">
 <div class="container py-5">
     <div class="row">
         <div class="col-md-12">
-            <h2 class="text-center text-white mb-4">Code de vérification envoyé, consultez votre boite mail !</h2>
-            <div class="row">
-                <div class="col-md-6 mx-auto">
-                </div>
-            </div>
+            <h2 class="text-center text-white mb-4">Code de vérification envoyé, consultez votre boite mail ! Lorsque votre email aura été vérifié, revenez sur cette page...</h2>
         </div>
     </div>
 </div>
