c8fc1f223bff8068b9a2bf54bd997d3d4f07e25f
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-03-29 22:18:50 +0100

    modified:   back/verification_email.php

--- back/verification_email.php
+++ back/verification_email.php
@@ -70,9 +70,16 @@ include('../includes/phpmailer.php');
         }
     }
 
-include('../includes/head.php');
-include('../includes/header.php');
+
 ?>
+<!-- Intégration de la police d'écriture  -->
+<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+<!-- Intégration Bootstrap 5  -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<!-- Intégration de notre CSS -->
+<link type="text/css" rel="stylesheet" href="../css/style.css">
+
 
 <script>
     var timer = null;
