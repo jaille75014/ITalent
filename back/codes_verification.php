902575129b31bfb246b6393fe8ebe1f61b17bdeb
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-04-02 14:56:45 +0200

    uhui

--- back/codes_verification.php
+++ back/codes_verification.php
@@ -50,8 +50,7 @@ integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLAS
     $req->execute([
         'id'=> htmlspecialchars($_GET['id'])
     ]);
-    $result = $req->fetchAll(PDO::FETCH_ASSOC);
-    var_dump($result);
+    $result = $req->fetch(PDO::FETCH_ASSOC);
 
     if(isset($result)){
         if(($result['value'] == $_POST['code']) && ($result['date'] >= $_GET['date'])){
