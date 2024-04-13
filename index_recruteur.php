26c7013b2dd2eeb2d20cb878dfa2fcb1c8215ab6
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-04-13 20:55:06 +0200

    xx

--- index_recruteur.php
+++ index_recruteur.php
@@ -1,15 +1,30 @@
 <?php 
     session_start(); 
     include('includes/header_location.php');
+    include('includes/fonctions_logs.php');
+    include("includes/bd.php");
     if (!isset($_SESSION['statut']) || $_SESSION['statut'] != 2) {
         redirectFailure('index.php', 'Vous n\'avez pas les droits pour accéder à cette page.');
     }
     if(!isset($_SESSION['captcha'])){
         redirectFailure('captcha.php', 'Chipeur arrête de chipper !');
-    } ?>
+    } 
+    
+    // Requete pour récuperer le nom, prénom, la ville, le numéro de téléphone, l'email, l'image et le nom du job
+$get_infos = 'SELECT USERS.user_id, USERS.lastname, USERS.firstname, USERS.city, USERS.phone, USERS.email, USERS.image, JOBS.name FROM USERS INNER JOIN JOBS ON USERS.student_job = JOBS.id WHERE USERS.status = 1 LIMIT 20';
+$req = $bdd->prepare($get_infos);
+$req->execute();
+$donnees = $req->fetchAll(PDO::FETCH_ASSOC);
+
+// mélanger les users
+shuffle($donnees);
+
+    
+    
+?>
 
 <!DOCTYPE html>
-<html lang="fr">
+<html>
 
 <?php 
 $title='Recruteur';
@@ -52,17 +67,42 @@ include('includes/head.php');?>
             <h1 class="text-center">CV Etudiants</h1>
         </div>
 
-        <div class="container">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">CV de ... (nom de l'étudiant)</h5>
-                    <p class="card-text">Tinder card avec pdf dedans</p>
-                    <object class="card-body" data="SyllabusDuProjet.pdf" type="application/pdf">
-                        <p>CV de <a href="SyllabusDuProjet.pdf">nom étudiant</a></p>
-                    </object>
+        <div class="list">
+        <?php foreach ($donnees as $user): ?>
+            <div class="line">
+                <div class="user">
+                    <div class="profile">
+                        <img src="<?= $user['image'] ?>" alt="photo de profil">
+                    </div>
+                    <div class="details">
+                        <h1 class="name"><?=$user['lastname'] . ' ' . $user['firstname']?></h1>
+                        <h3 class="username"><?php $user['email'] ?></h3>
+                    </div>
+                </div>
+                <div class="status">
+                    <span></span>
+                    <p><?= $user['name'] == 0 ? 'Type de contrat non précisé' : $user['name']; ?></p>                
+                </div>
+                <div class="location">
+                    <p><?= $user['city'] ?></p>
+                </div>
+                <div class="phone">
+                    <p><?= $user['phone'] ?></p>
+                </div>
+                <div class="contact">
+                    <a href="messagerie.php?user_id=<?= $user['user_id'] ?>" class="btn btn-primary">Contacter</a>
+                </div>
+                <div class="action">
+                    <div class="icon">
+                        <input type="hidden" id="user_followed" value="<?= $user['user_id'] ?>">
+                        <input type="hidden" id="user_follower" value="<?= $_SESSION['user_id'] ?>">
+                        <button onclick="follow()"><i class="bi bi-person-fill-add"></i></button>
+                    </div>
                 </div>
             </div>
-        </div>
+        <?php endforeach; ?>
+    </div>
+    <script src="js/script.js"></script>
 
 
         <script src="js/script.js"></script>
