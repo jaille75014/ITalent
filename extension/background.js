c1207edf472b498da164e1e77a611129b6c1f18a
Albann0 <131862757+Albann0@users.noreply.github.com>
2024-05-02 22:51:40 +0200

    zef

--- extension/background.js
+++ extension/background.js
@@ -1,2 +1,2 @@
-chrome.browserAction.setBadgeText({text: 'ON'}); // Affiche le badge "ON" sur l'icône de l'extension
-chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'}); // Couleur du badge
+chrome.action.setBadgeText({text: 'ON'}); // Affiche le badge "ON" sur l'icône de l'extension
+chrome.action.setBadgeBackgroundColor({color: '#4688F1'}); // Couleur du badge
