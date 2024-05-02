81a23f42105c79937d5c008629e3d673051a375b
Albann0 <131862757+Albann0@users.noreply.github.com>
2024-05-02 10:20:17 +0200

    saz

--- js/header.js
+++ js/header.js
@@ -1,9 +1,6 @@
-const toggler = document.getElementById("toggler");
-const nav = document.getElementById("nav");
-
-toggler.addEventListener("click",toggleNav);
-
-function toggleNav(){
-    toggler.classList.toggle("active");
-    nav.classList.toggle("active");
-}
+const menuHamburger = document.querySelector(".burger-menu")
+        const navLinks = document.querySelector(".nav-links")
+ 
+        menuHamburger.addEventListener('click',()=>{
+        navLinks.classList.toggle('mobile-menu')
+        })
