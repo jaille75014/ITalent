2314b54aa7c66b9023a37d1a904a9582d1cc7081
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-05-05 13:23:26 +0200

    oih

--- back/is_new_messages.php
+++ back/is_new_messages.php
@@ -6,7 +6,8 @@ $user_id = $_SESSION['user_id'];
 
 $new_messages = $bdd->prepare('SELECT count(*) FROM MESSAGE WHERE read_message = 0 AND user_id_target_id = ?');
 $new_messages->execute([$user_id]);
-$new_messages = $new_messages->fetchAll();
+$new_messages = $new_messages->fetchColumn();
+
 
 header('Content-Type: application/json');
 echo json_encode($new_messages);
