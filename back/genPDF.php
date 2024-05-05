aebb27f8ed4799fdfcf9082cf1938fd4a8ed59da
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-05-05 10:40:43 +0200

    cdom,

--- back/genPDF.php
+++ back/genPDF.php
@@ -26,6 +26,7 @@ $user_info = $user_info_q->fetchAll(PDO::FETCH_ASSOC);
 
 // Créer le PDF
 $pdf = new FPDF('P','mm','A4');
+
 $pdf->AddPage();
 $pdf->SetFont('Helvetica','B',16);
 
@@ -39,22 +40,28 @@ $pdf->Cell(59,5,'',0,0);
 $pdf->Cell(59,5,'Informations :',0,1);
 
 $pdf->SetFont('Helvetica','',16);
-$pdf->Cell(130,5,'France, FR',0,0);
-$pdf->Cell(25,5,'Nom : ',0,0);
-$pdf->Cell(34,5,$user_info['lastname'],0,1);
 
-$pdf->Cell(130,5,$user_info['city'] . ', ' . $userInfo['zip'],0,0);
-$pdf->Cell(25,5,'Prénom : ',0,0);
-$pdf->Cell(34,5,$user_info['firstname'],0,1);
+if(empty($user_info)) {
+    $pdf->Cell(0,10,'Informations de l\'utilisateur non disponibles',0,1);
+} else {
+    $user_info_first = $user_info[0];
 
-$pdf->Cell(130,5,'',0,0);
-$pdf->Cell(25,5,'Email : ',0,0);
-$pdf->Cell(34,5,$user_info['email'],0,1);
+    $pdf->Cell(130,5,'France, FR',0,0);
+    $pdf->Cell(25,5,'Nom : ',0,0);
+    $pdf->Cell(34,5,iconv('UTF-8', 'windows-1252', $user_info_first['lastname']),0,1);
 
-$pdf->Cell(130,5,'',0,0);
-$pdf->Cell(25,5,'Téléphone : ',0,0);
-$pdf->Cell(34,5,$user_info['tel'],0,1);
+    $pdf->Cell(130,5,iconv('UTF-8', 'windows-1252', $user_info_first['city']) . ', ' . $user_info_first['zip'],0,0);
+    $pdf->Cell(25,5,'Prénom : ',0,0);
+    $pdf->Cell(34,5,iconv('UTF-8', 'windows-1252', $user_info_first['firstname']),0,1);
 
+    $pdf->Cell(130,5,'',0,0);
+    $pdf->Cell(25,5,'Email : ',0,0);
+    $pdf->Cell(34,5,$user_info_first['email'],0,1);
+
+    $pdf->Cell(130,5,'',0,0);
+    $pdf->Cell(25,5,'Téléphone : ',0,0);
+    $pdf->Cell(34,5,$user_info_first['tel'],0,1);
+}
 
 $pdf->SetFont('Helvetica','B',15);
 $pdf->Cell(130,5,'Mes compétences',0,0);
@@ -81,11 +88,11 @@ if(empty($user_info)) {
     $counter = 1;
     foreach ($user_info as $row) {
         $pdf->Cell(20,6,$counter++,1,0, 'C');
-        $pdf->Cell(80,6,$row['competence_name'],1,0, 'C');
+        $pdf->Cell(80,6,iconv('UTF-8', 'windows-1252', $row['competence_name']),1,0, 'C');
         $pdf->Cell(50,6,$row['level'],1,0, 'C');
         $pdf->Cell(39,6,$row['validity'],1,1, 'C');
     }
 }
 
 $pdf->Output();
-?> 
+?>
