2af08f2c2cb1f41d59b78199152fce483fd17a54
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-05-05 13:16:11 +0200

    exoih

--- back/is_new_messages.php
+++ back/is_new_messages.php
@@ -1,8 +1,12 @@
 <?php 
-
+session_start();
 include('../includes/bd.php');
 
-$new_messages = $bdd->query('SELECT * FROM MESSAGE WHERE read_message = 0')->fetchAll();
+$user_id = $_SESSION['user_id'];
+
+$new_messages = $bdd->prepare('SELECT count(*) FROM MESSAGE WHERE read_message = 0 AND user_id_target_id = ?');
+$new_messages->execute([$user_id]);
+$new_messages = $new_messages->fetchAll();
 
 header('Content-Type: application/json');
 echo json_encode($new_messages);
