206e4bfd9e5506e6446501ccbb5cd541ff09776b
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-05-05 12:32:22 +0200

    cod,

--- back/is_new_messages.php
+++ back/is_new_messages.php
@@ -2,7 +2,7 @@
 
 include('../includes/bd.php');
 
-$new_messages = $bdd->query('SELECT * FROM MESSAGE WHERE read_message IS NULL')->fetchAll();
+$new_messages = $bdd->query('SELECT * FROM MESSAGE WHERE read_message = 0')->fetchAll();
 
 header('Content-Type: application/json');
 echo json_encode($new_messages);
