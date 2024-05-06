7ecbb7464e0b7caf4cf5a7b8603311379c5773a6
jaille75014 <jules91@gmx.com>
2024-05-06 19:36:29 +0200

    modified:   admin_log.php

--- admin_log.php
+++ admin_log.php
@@ -39,14 +39,39 @@ include('includes/head.php');?>
     <div class="container">
         <h3>Logs du site</h3>
 
-        <p>Logs d'erreur Apache :</p>
-        <pre><?php echo htmlspecialchars($apache_error_logs); ?></pre>
-
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Logs d'erreur Apache :</h5>
+                <?php if (!empty($apache_error_logs)): ?>
+                    <?php
+                    $log_entries = explode("\n", $apache_error_logs);
+                    ?>
+                    <div class="accordion" id="logAccordion">
+                        <?php foreach ($log_entries as $index => $log_entry): ?>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="logHeading<?php echo $index; ?>">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#logCollapse<?php echo $index; ?>" aria-expanded="false" aria-controls="logCollapse<?php echo $index; ?>">
+                                        Log <?php echo $index + 1; ?>
+                                    </button>
+                                </h2>
+                                <div id="logCollapse<?php echo $index; ?>" class="accordion-collapse collapse" aria-labelledby="logHeading<?php echo $index; ?>" data-bs-parent="#logAccordion">
+                                    <div class="accordion-body">
+                                        <?php echo htmlspecialchars($log_entry); ?>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                <?php else: ?>
+                    <p>Aucun log d'erreur Apache disponible.</p>
+                <?php endif; ?>
+            </div>
+        </div>
 
         <div class="mt-3">
             <h5>Télécharger les logs</h5>
             <form action="back/download_logs.php" method="post">
-                <button type="submit" class="btn btn-primary mb-6">Télécharger les logs</button>
+                <button type="submit" class="btn btn-primary mb-3 mt-3">Télécharger les logs</button>
             </form>
         </div>
     </div>
