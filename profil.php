020ab7d40e0152bfa7769de1266bed41b1e2c05e
Albann0 <131862757+Albann0@users.noreply.github.com>
2024-05-07 16:13:30 +0200

    new ttab

--- profil.php
+++ profil.php
@@ -120,10 +120,10 @@ $url = 'profil'; //Permet de revenir sur cette page en cas d'erreurs dans les pa
                                 
                                 if ($userInfo['statut'] == '1'): 
                                     if (file_exists($filename)): ?>
-                                        <a href="<?php echo $public_url; ?>" class="btn btn-primary mt-3">Afficher le CV</a>
-                                        <a href="back/genPDF.php?reload=1" class="btn btn-primary mt-3">Regénérer le CV en PDF</a>
+                                        <a href="<?php echo $public_url; ?>" target="_blank" class="btn btn-primary mt-3">Afficher le CV</a>
+                                        <a href="back/genPDF.php?reload=1" class="btn btn-primary mt-3" target="_blank">Regénérer le CV en PDF</a>
                                     <?php else: ?>
-                                        <a href="back/genPDF.php" class="btn btn-primary mt-3">Télécharger le CV en PDF</a>
+                                        <a href="back/genPDF.php" target="_blank" class="btn btn-primary mt-3">Télécharger le CV en PDF</a>
                                     <?php endif; 
                                 endif; ?>
                                 <a href="back/supp_compte" class="btn btn-danger mt-3" onclick="return confirm('Êtes-vous sûr de vouloir supprimer votre compte ? Cette action est irréversible.');">Supprimer mon compte</a>
