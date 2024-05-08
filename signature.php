216778b79d677507cc1a3d32a03d9159b1a2e80a
jaille75014 <jules91@gmx.com>
2024-05-08 11:37:08 +0200

    modified:   signature.php

--- signature.php
+++ signature.php
@@ -1,7 +1,9 @@
-<h3 class="text-center" >Entrer une signature : </h3>
-<canvas id="signatureCanva" width="400" height="200" class="bg-white"></canvas>
-<form id="signatureFormulaire" action="back/genPDF<?=$_GET['reload']=='1'?'?reload=1':'' ;?>" method="post">
+<h3 class="text-center">Entrer une signature : </h3>
+<div id="signatureContainer" class="text-center">
+    <canvas id="signatureCanvas" width="400" height="200" class="bg-white border border-dark"></canvas>
+</div>
+<form id="signatureForm" action="back/genPDF<?=$_GET['reload']=='1'?'?reload=1':'' ;?>" method="post">
     <input type="hidden" id="signatureBase64" name="signatureBase64">
     <button type="button" onclick="saveSignature()">Enregistrer</button>
 </form>
-<script src="js/canva.js"></script>
+<script src="js/canvas.js"></script>
