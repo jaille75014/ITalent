9b709a63b5ce4f129f416d214db024a09825653e
Raffaello <113721632+Raffaello-rgb@users.noreply.github.com>
2024-05-16 12:18:41 +0200

    c

--- delete_users_cron.php
+++ delete_users_cron.php
@@ -1,5 +1,10 @@
 <?php 
-include('includes/bd.php');
+try {
+    $bdd = new PDO('mysql:host=localhost:3306;dbname=italent', 'root', 'AJR3MOUSQUETAIRES');
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Erreur :' . $e->getMessage());
+}
 include('includes/phpmailer.php');
 
 $delete_user = 'SELECT user_id, date_ban FROM BAN';
